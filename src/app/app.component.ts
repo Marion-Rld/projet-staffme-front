9859d0366dea91810ffdf511afa09e6fcaa71914
matz57 <matz.jere.pro@gmail.com>
2024-05-01 18:16:24 +0200

    :lipstick: Commit Branch Dev + menu beginning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from "./main-nav/main-nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [RouterOutlet, MainNavComponent]
 })
 export class AppComponent {
   title = 'projet-staffme-front';
