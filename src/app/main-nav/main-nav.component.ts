943ae627ac8e4c5265c8d5e237e2a9a65b3c8d5d
matz57 <matz.jere.pro@gmail.com>
2024-05-10 18:58:21 +0200

    :sparkles: Beginning of desktop menu and mobile menu

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,34 +1,18 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrl: './main-nav.component.scss',
   standalone: true,
-  imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    AsyncPipe,
-  ]
+  imports: [CommonModule],
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
-  private breakpointObserver = inject(BreakpointObserver);
+  hideNav = false;
+  showMobileMenu = false;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  toggleNav() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 }
