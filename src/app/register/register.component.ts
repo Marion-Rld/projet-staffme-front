c99f07e142bafe8d4d124be12cf4cf47c98ea3ce
Marion Rollando <rollando.marion@gmail.com>
2024-05-14 11:50:58 +0200

    🦺 Add validation to register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule],
+  imports: [MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  firstname: string = '';
-  lastname: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-
-  constructor() { }
+  registerForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    lastname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
 
   onSubmit() {
-    console.log('Form submitted');
-    console.log('First Name: ' + this.firstname);
-    console.log('Last Name: ' + this.lastname);
-    console.log('Email: ' + this.email);
-    console.log('Password: ' + this.password)
+    if (this.registerForm.valid) {
+      console.log('Form Data:', this.registerForm.value);
+    } else {
+      console.log('Form is not valid');
+    }
   }
 
+  passwordsMatch(): boolean {
+    const pass = this.registerForm.get('password')?.value; 
+    const confirmPass = this.registerForm.get('confirmPassword')?.value; 
+    return pass === confirmPass;
+  }
 }
