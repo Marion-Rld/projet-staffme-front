bbdc2e2665607a7ad68ec228244b83848e69f9a6
Marion Rollando <rollando.marion@gmail.com>
2024-05-15 14:02:56 +0200

    ✨ Add HttpClientModule and ApiService to support HTTP operations + update formControlName conventions in register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { MainNavComponent } from "./features/main-nav/main-nav.component";
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [RouterOutlet, MainNavComponent, MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule]
+    imports: [RouterOutlet, MainNavComponent, MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
 })
 export class AppComponent {
   title = 'projet-staffme-front';
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ApiService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // USERS
+  getUsers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/users-api/users`);
+  }
+
+  addUser(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/users-api/user`, userData);
+  }
+
+  updateUser(userId: string, userData: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/users-api/user/${userId}`, userData);
+  }
+
+  deleteUser(userId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/users-api/user/${userId}`);
+  }
+
+  // SKILLS
+  getSkills(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/skills-api`);
+  }
+
+  // TEAMS
+  getTeams(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/teams-api`);
+  }
+
+  // PROJECTS
+  getProjects(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/projects-api`);
+  }
+
+  // DOCUMENTS
+  getDocuments(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/documents-api`);
+  }
+
+  // SKILL LEVELS
+  getSkillLevels(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/skill-levels-api`);
+  }
+}
