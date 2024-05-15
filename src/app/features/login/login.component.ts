ede31bc79cba026620ab19e4b08244c0e21ce818
Marion Rollando <rollando.marion@gmail.com>
2024-05-15 09:36:22 +0200

    🦺 Add validations in login form

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,25 +1,33 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, FormGroup,
+  FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+  import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule],
+  imports: [MatInputModule, MatButtonModule,CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  email: string = '';
-  password: string  = '';
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
 
   constructor() {
     console.log('LoginComponent constructor');
   }
 
   onSubmit() {
-    console.log('LoginComponent onSubmit', this.email, this.password);
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+    } else {
+      console.log('Form is not valid');
+    }
   }
 }
