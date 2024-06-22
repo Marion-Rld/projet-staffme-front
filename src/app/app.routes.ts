c283a7d96653615fc79c8271b0154b81f512174f
Marion Rollando <rollando.marion@gmail.com>
2024-06-22 16:48:28 +0200

    ✨ Create projects component + add projects route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,19 @@ import { RegisterComponent } from './features/register/register.component';
 import { ForgotPasswordComponent } from './features/forgot-password/forgot-password.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { AuthGuard } from '../../src/app/guard/auth.guard';
-/*import { ProjectsComponent } from './features/projects/projects.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+/*
 import { TeamsComponent } from './features/teams/teams.component';
 import { CollaboratorsComponent } from './features/collaborators/collaborators.component';
 import { LogoutComponent } from './features/logout/logout.component';*/
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent},
-    { path: 'forgot-password', component: ForgotPasswordComponent},
-    { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
-    /*{ path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard] },
+  /*
     { path: 'teams', component: TeamsComponent, canActivate: [AuthGuard] },
     { path: 'collaborators', component: CollaboratorsComponent, canActivate: [AuthGuard] },
     { path: 'logout', component: LogoutComponent, canActivate: [AuthGuard] }*/
