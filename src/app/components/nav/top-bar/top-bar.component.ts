c5add7cec89c09b1d51f5721c3c18eb3f033e3c9
Marion Rollando <rollando.marion@gmail.com>
2024-07-07 15:20:00 +0200

    ✨ Add regex to display project when url is projects/:id

--- src/app/components/nav/top-bar/top-bar.component.ts
+++ src/app/components/nav/top-bar/top-bar.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule, NavigationEnd, Event as RouterEvent } from '@angular/router';
+import {
+  Router,
+  RouterModule,
+  NavigationEnd,
+  Event as RouterEvent,
+} from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -7,30 +12,36 @@ import { filter, map } from 'rxjs/operators';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+  styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent {
   title: string = '';
   userName: string = '';
 
   constructor(private router: Router) {
-    this.router.events.pipe(
-      filter((event: RouterEvent): event is NavigationEnd => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      if (event.url === '/') {
-        this.title = 'Tableau de bord';
-      } else if(event.url === '/projects') {
-        this.title = 'Projets';
-      } else if(event.url === '/teams') {
-        this.title = 'Équipes';
-      } else if (event.url === '/collaborators') {
-        this.title = 'collaborateurs';
-      } else if (event.url === '/user-list') {
-        this.title = 'Liste des utilisateurs';
-      }
-    });
+    this.router.events
+      .pipe(
+        filter(
+          (event: RouterEvent): event is NavigationEnd =>
+            event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event: NavigationEnd) => {
+        if (event.url === '/') {
+          this.title = 'Tableau de bord';
+        } else if (event.url === '/projects') {
+          this.title = 'Projets';
+        } else if (event.url === '/teams') {
+          this.title = 'Équipes';
+        } else if (event.url === '/collaborators') {
+          this.title = 'collaborateurs';
+        } else if (event.url === '/user-list') {
+          this.title = 'Liste des utilisateurs';
+        } else if (/^\/projects\/[^\/]+$/.test(event.url)) {
+          this.title = 'Projet';
+        }
+      });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
