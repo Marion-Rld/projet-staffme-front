595457c0ed25f21e6a4db8cb06d144ce17236490
Marion Rollando <rollando.marion@gmail.com>
2024-07-10 13:34:36 +0200

    ✨ Add method to load userData

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrls: ['./user-profile.component.scss'],
+  providers: [UserService],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  userId: string = '';
+  userData: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.userId = params['id'];
+      this.loadUserData();
+    });
+  }
+
+  loadUserData() {
+    this.userService.getUserById(this.userId).subscribe((data) => {
+      this.userData = data;
+
+      console.log('User data:', this.userData);
+    });
+  }
 }
