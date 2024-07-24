d56302dfacdfc13d4ac3b68782c20df2008d3a85
matz57 <matz.jere.pro@gmail.com>
2024-07-24 17:53:54 +0200

    :poop: Test for csrf protection

--- src/app/csrf.interceptor.ts
+++ src/app/csrf.interceptor.ts
@@ -1,13 +1,38 @@
 import { inject } from '@angular/core';
-import { HttpEvent, HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
+import { HttpEvent, HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
-export const csrfInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
-  const cookieService = inject(CookieService);
-  const csrfToken = cookieService.get('XSRF-TOKEN');
-  const cloned = req.clone({
-    headers: req.headers.set('X-XSRF-TOKEN', csrfToken)
-  });
-  return next(cloned);
+export const csrfInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+  const http = inject(HttpClient);
+  let csrfToken: string | null = localStorage.getItem('csrfToken');
+
+  if (csrfToken) {
+    req = req.clone({
+      setHeaders: {
+        'X-CSRF-Token': csrfToken
+      }
+    });
+  }
+
+  return next(req).pipe(
+    catchError((error: any) => {
+      if (error.status === 403 && error.error.message === 'Invalid CSRF token') {
+        return http.get<any>('/csrf-token').pipe(
+          switchMap((token: any) => {
+            csrfToken = token.csrfToken;
+            localStorage.setItem('csrfToken', csrfToken ?? '');
+            const clonedRequest = req.clone({
+              setHeaders: {
+                'X-CSRF-Token': csrfToken!
+              }
+            });
+            return next(clonedRequest);
+          })
+        );
+      } else {
+        return throwError(() => error);
+      }
+    })
+  );
 };
